cd557d266ec021ee02d8f33c22b97c4ef03a0b74
hyuns66 <hyuns6677@gmail.com>
2023-06-12 04:36:03 +0000

    fix: nested_unet.py
    delete if else statemensts in nested_unet.py
    convert nested_unet module always operate as deep_supervision mode:

--- static_quantization.py
+++ static_quantization.py
@@ -20,11 +20,13 @@ if __name__=="__main__":
     args = get_args()
     
     # Prepare input data
-    input_data = torch.full((1, 3, 1024, 1024), 0.5)
+    input_data = torch.full((1, 3, 512, 512), 0.5).cuda()
     
     model_path = os.path.join(args.model_dir, args.model_path)
     save_path = os.path.join("./quantized_models", os.path.splitext(args.model_path)[0]+"_quantized.pt")
-    model = torch.load(model_path).cpu()
+    model = mm.NestedUNet(deep_supervision=True).cuda()
+    model.load_state_dict(torch.load(model_path).state_dict())
+    # model = torch.load(model_path).cuda()
     # Make predictions
     with torch.no_grad():
         output = model(input_data)
@@ -35,10 +37,14 @@ if __name__=="__main__":
     print("mean : ", torch.mean(output))
 
     backend = "qnnpack"
+    model = model.cpu()
     model.qconfig = torch.quantization.get_default_qconfig(backend)
     torch.backends.quantized.engine = backend
     model_static_quantized = torch.quantization.prepare(model, inplace=False)
-    model_static_quantized = torch.quantization.convert(model_static_quantized, inplace=False)
+    model_static_quantized = torch.quantization.convert(model_static_quantized, inplace=False).cuda()
+    # model_traced = torch.jit.trace(model_static_quantized, torch.quantization.QuantStub(input_data))
+    # print(model_traced)
+    # exit()
     model_script = torch.jit.script(model_static_quantized)
     torch.jit.save(model_script, save_path)
     model_script = torch.jit.load(save_path)
